d0505cabc28e8f7779309b40fbefde900a162ab5
adeemwt <admwt55@gmail.com>
2016-03-30 12:35:30 +0300

    Update Trainer.h

--- FootBall_Scheduler_Simulator/Trainer.h
+++ FootBall_Scheduler_Simulator/Trainer.h
@@ -13,7 +13,7 @@ public:
 	// c-tors, d-tors
 	Trainer(const char* name, const char* f_name, long id) 
 		: Person(name, f_name, id) { }
-	~Trainer()			{delete name;}
+	~Trainer()			{delete []name;}
 	
 	// Operators
 	const Trainer operator+=()	{MakeDecision();}
