d022f3c80dcea2837b1a32312f8fd73cc9e11131
adeemwt <admwt55@gmail.com>
2016-03-30 12:27:00 +0300

    Update Referee.h

--- FootBall_Scheduler_Simulator/Referee.h
+++ FootBall_Scheduler_Simulator/Referee.h
@@ -14,7 +14,7 @@ public:
 	Referee(const char* name, const char* f_name, long id) 
 		: Person(name, f_name, id) { }
 	
-	~Referee()			{~name;}
+	~Referee()			{delete name;}
 	// Operators
 	
 	// Methods
