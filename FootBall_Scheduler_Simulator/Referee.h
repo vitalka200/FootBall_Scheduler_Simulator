013b58fa91d4bf0a8dc2e51c8eeca79eac3a8996
adeemwt <admwt55@gmail.com>
2016-03-30 12:18:42 +0300

    Update Referee.h

--- FootBall_Scheduler_Simulator/Referee.h
+++ FootBall_Scheduler_Simulator/Referee.h
@@ -10,11 +10,17 @@ public:
 	enum CardType {YELLOW, RED};
 	static const char* CardNames[];
 	// c-tors, d-tors
-
+	
+	Referee(const char* name, const char* f_name, long id) 
+		: Person(name, f_name, id) { }
+	
+	~Referee()			{~name;}
 	// Operators
-
+	
 	// Methods
-
+	GiveCardAccordigntoAction(Player::Action Act)	{}    //Needed?
+	GiveCardonRandom()				{}
+	
 	// Method overrides
 	friend std::ostream& operator<<(std::ostream& os, const CardType& ct)
 	{os << Referee::CardNames[ct]; return os;}
