d2ece519a554c564cc0ec872c2654deae26a616f
adeemwt <admwt55@gmail.com>
2016-03-30 12:26:32 +0300

    Update Trainer.h

--- FootBall_Scheduler_Simulator/Trainer.h
+++ FootBall_Scheduler_Simulator/Trainer.h
@@ -3,19 +3,26 @@
 
 #include <iostream>
 #include "Person.h"
-
+#include "Team"
 class Trainer: Person
 {
 public:
 	enum TrainerDecision {CHANGE_PLAYER, MOVE_PLAYER};
 	static const char* TrainerDecisionNames[];
+	Team	m_team;
 	// c-tors, d-tors
 	Trainer(const char* name, const char* f_name, long id) 
 		: Person(name, f_name, id) { }
+	~Trainer()			{delete name;}
+	
 	// Operators
-
+	const Trainer operator+=()	{MakeDecision();}
+	
 	// Methods
 	TrainerDecision MakeDecision();
+	addTeam(Team& team);
+	
+	
 	// Methods overrides
 	friend std::ostream& operator<<(std::ostream& os, const TrainerDecision& td)
 	{os << Trainer::TrainerDecisionNames[td]; return os;}
