0650852f9293b234ddece642ebf14b2455c00a05
Karabitski, Vitali <vitalka200@gmail.com>
2016-04-01 08:53:16 +0300

    Main implementation

--- FootBall_Scheduler_Simulator/MainApp.cpp
+++ FootBall_Scheduler_Simulator/MainApp.cpp
@@ -3,8 +3,23 @@
 
 using namespace std;
 
-
 void main()
 {
+	GameManager* gm = new GameManager();
 
+	gm->ManageTeams();
+	gm->ManageReferees();
+	gm->ManageStadiums();
+
+	gm->ManageGames();
+	gm->ManageGameSchedule();
+
+	gm->SellTickets();
+	gm->SendInvitations();
+
+	gm->StartGame();
+	gm->GetStats();
+	gm->GetMonthSummary();
+
+	delete gm;
 }
