c47f362a349f482228026d7628b893058b69ce5e
Karabitski, Vitali <vitalka200@gmail.com>
2016-04-01 11:04:00 +0300

    Remove unneeded operators and data members

--- FootBall_Scheduler_Simulator/Trainer.h
+++ FootBall_Scheduler_Simulator/Trainer.h
@@ -3,23 +3,19 @@
 
 #include <iostream>
 #include "Person.h"
-#include "Team"
 class Trainer: Person
 {
 public:
 	enum TrainerDecision {CHANGE_PLAYER, MOVE_PLAYER};
 	static const char* TrainerDecisionNames[];
-	Team	m_team;
 	// c-tors, d-tors
 	Trainer(const char* name, const char* f_name, long id) 
 		: Person(name, f_name, id) { }
 	
 	// Operators
-	const Trainer operator+=()	{MakeDecision();}
 	
 	// Methods
 	TrainerDecision MakeDecision();
-	addTeam(Team& team);
 	
 	
 	// Methods overrides
