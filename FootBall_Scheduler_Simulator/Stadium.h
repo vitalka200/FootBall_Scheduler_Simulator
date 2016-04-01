b2455bb4a857ebbc6c631e672175493d10a3d49e
Karabitski, Vitali <vitalka200@gmail.com>
2016-04-01 11:05:14 +0300

    Added GamesByTimeDateNode to be able to create schedule list in stadium. Added some essentials methods to work with stadium

--- FootBall_Scheduler_Simulator/Stadium.h
+++ FootBall_Scheduler_Simulator/Stadium.h
@@ -8,24 +8,52 @@ class TimeAndDate;
 class Time;
 class Date;
 
+class GamesByTimeDateNode
+{
+public:
+	GamesByTimeDateNode(const Date& date)
+		: m_date(NULL), m_times(NULL), m_games(NULL)      { m_date = new Date(date);}
+	~GamesByTimeDateNode()                                { delete []m_date; }
+	//Methods
+	void         AddGame(Game* g);
+	void         RemoveGame(const Game* g);
+	const Date*  GetDate()                          const { return m_date; }
+	// operators
+	const GamesByTimeDateNode& operator+=(Game* g)        { AddGame(g);    return *this; }
+	const GamesByTimeDateNode& operator-=(const Game* g)  { RemoveGame(g); return *this; }
+private:
+	Date*  m_date;
+	Time*  m_times;
+	Game*  m_games;
+
+	// Disable copy and assigment
+	GamesByTimeDateNode(const GamesByTimeDateNode& gt);
+	const GamesByTimeDateNode& operator=(const GamesByTimeDateNode& gt);
+
+};
+
 class Stadium
 {
 public:
 	// c-tors, d-tors
 	Stadium(const char* name, int maxFans)
 		: m_name(NULL),  m_maxFans(maxFans),
-		  m_games(NULL), m_schedule(NULL)      { SetName(name); };
+		  m_gameList(NULL)                     { SetName(name); };
 	Stadium(const Stadium& s)
-		: m_name(NULL), m_games(NULL),
-		  m_schedule(NULL)                     { *this = s; }
-	~Stadium()                                 { delete []m_name; delete[] m_schedule; }
+		: m_name(NULL), m_gameList(NULL)       { *this = s; }
+	~Stadium()                                 { delete []m_name; delete[] m_gameList; }
 	// Operators
 	const Stadium& operator=(const Stadium& s);
 	// Methods
-	void        SetName(const char* name)      { delete []m_name; m_name = strdup(name); }
-	void        SetMaxFans(int maxFans)        { m_maxFans = maxFans; }
-	const char* GetName()                const { return m_name; }
-	const int   GetMaxFans()             const { return m_maxFans; }
+	void        SetName(const char* name)                          { delete []m_name; m_name = strdup(name); }
+	void        SetMaxFans(int maxFans)                            { m_maxFans = maxFans; }
+	const char* GetName()                                    const { return m_name; }
+	const int   GetMaxFans()                                 const { return m_maxFans; }
+	Game*       GetGameByTimeAndDate(const TimeAndDate& tad) const ;
+	Game*       GetGamesByDate(const Date& d)                const ;
+	void        AddGame(Game* g);
+	void        RemoveGame(Game* g);
+	void        MoveGameTime(Game* g, const TimeAndDate& newTad);
 	
 	// Method overrides
 	friend std::ostream& operator<<(std::ostream& os, const Stadium& s);
@@ -33,8 +61,7 @@ private:
 	char* m_name;
 	int   m_maxFans;
 	
-	TimeAndDate* m_schedule;
-	Game*        m_games;
+	GamesByTimeDateNode* m_gameList;
 };
 
 
