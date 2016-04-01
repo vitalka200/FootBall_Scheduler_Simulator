9904cf48afe050f647a97ba2f0778b86aebdec01
Karabitski, Vitali <vitalka200@gmail.com>
2016-04-01 11:00:55 +0300

    Added more operators and methods to Game.h

--- FootBall_Scheduler_Simulator/Game.h
+++ FootBall_Scheduler_Simulator/Game.h
@@ -7,33 +7,21 @@
 class Team;
 class Referee;
 class Player;
-
-struct PlayerStats
-{
-public:
-	Player*                 pl;
-	Player::PlayerMovement* moves;
-	int                     yellowCards;
-	int                     redCards;
-	int                     moves_number;
-	int 			numOfPasses;
-	int			numOfGoal;
-	~PlayerStats()          {delete []moves;}
-private:
-};
+class PlayerStats;
 
 class Game
 {
 public:
 	// c-tors, d-tors
 	Game(Team* team, Referee* ref, int maxFans)
-		: m_teams(NULL), m_referees(NULL),
-		  m_maxFans(maxFans), m_timeAndDate(NULL)   { }
+		: m_teams(NULL), m_referees(NULL), 
+		m_maxFans(maxFans), m_timeAndDate(NULL)   { m_refereesCount = 0; }
 	Game(const Game& g)
 		: m_teams(NULL), m_referees(NULL),
 		  m_timeAndDate(NULL)                       { *this = g; }
 	~Game()                                         { delete []m_playerStats; delete []m_referees; }
 	// Operators
+	bool        operator==(const Game& g)     const ;
 	const Game& operator=(const Game& g);
 	const Game& operator+=(Team* t)                 { AddTeam(t);         return *this; }
 	const Game& operator-=(const Team* t)           { RemoveTeam(t);      return *this; }
@@ -50,8 +38,14 @@ public:
 	const Game& operator>>(const TimeAndDate& tad)  { RemoveTimeAndDate(tad); return *this; }
 
 	// Methods
-	const int  GetMaxFans()              const;
-	void       StartGame()               const;
+	int                GetMaxFans()                   const { return m_maxFans; }
+	int                GetRefereesCount()             const { return m_refereesCount; }
+	const PlayerStats* GetGameStats()                 const { return m_playerStats; }
+	const Team*        GetWinner()                    const ;
+	const Referee*     GetReferees()                  const { return m_referees; }
+	const TimeAndDate* GetTimeAndDate()               const { return m_timeAndDate; }
+
+	void       StartGame()                    const;
 
 	void       AddTeam(Team* t);
 	void       RemoveTeam(const Team* t);	
@@ -67,17 +61,17 @@ public:
 	void       AddTimeAndDate(const TimeAndDate& tad);
 	void       RemoveTimeAndDate(const TimeAndDate& tad);
 
-	const PlayerStats* GetGameStats()    const { return m_playerStats; }
 	// Method Overrides
 	friend std::ostream& operator<<(std::ostream& os, const Game& g);
 private:
-	Team	      winner;		//easier when the game is over to print the winner of the game.
+	int           m_gameScore[2];
 	Team*         m_teams;
 	Referee*      m_referees;
+	int           m_refereesCount;
 	int           m_maxFans;
 	PlayerStats*  m_playerStats;
 	TimeAndDate*  m_timeAndDate;
 };
-
+int m_gamescore[] = { 0, 0 };
 
 #endif
