4d798339aa7a9f3c25b1d54e52a7e7034b0ab448
Karabitski, Vitali <vitalka200@gmail.com>
2016-04-10 18:03:19 +0300

    Changed definition of Player stats. Added funtions/operators for Atack/Defence

--- FootBall_Scheduler_Simulator/Team.h
+++ FootBall_Scheduler_Simulator/Team.h
@@ -6,24 +6,36 @@
 // Forward declaration
 class  Player;
 class  Trainer;
+class  Referee;
 
 class  PlayerStats
 {
 public:
-	Player*                 pl;
-	Player::PlayerMovement* moves;
-	int                     yellowCards;
-	int                     redCards;
-	int                     moves_number;
-	int                     numOfPasses;
-	int                     numOfGoal;
+	PlayerStats(Player* pl)
+		: m_pl(NULL), m_moves(NULL)                   { m_redCards = 0; m_yellowCards = 0;
+	                                                    m_numOfGoal = 0; m_movesNumber = 0;
+														m_pl = pl; }
+	~PlayerStats()                                    { delete []m_moves; }
+	// Methods
+	void AddMove(const Player::PlayerMovement* move)  ;
+	void AddCard(const Referee::CardType* card)       { (&card) ? m_redCards++: m_yellowCards++; }
+	void AddGoals()                                   { m_numOfGoal++; }
 
-	PlayerStats() : moves(NULL) { }
-	~PlayerStats()              {delete []moves;}
+	int  GetYellowCards()                       const { return m_yellowCards; }
+	int  GetRedCards()                          const { return m_redCards; }
+	int  GetGoals()                             const { return m_numOfGoal; }
 private:
 	// Disable copy and equality
 	PlayerStats(const PlayerStats& ps);
 	const PlayerStats& operator=(const PlayerStats& ps);
+
+	// Data Members
+	Player*                 m_pl;
+	Player::PlayerMovement* m_moves;
+	int                     m_yellowCards;
+	int                     m_redCards;
+	int                     m_numOfGoal;
+	int                     m_movesNumber;
 };
 
 class Team
@@ -44,6 +56,9 @@ public:
 	const Team& operator-=(const Player* p)  { RemovePlayer(p);  return *this; }
 	const Team& operator+=(Trainer* t)       { AddTrainer(t);    return *this; }
 	const Team& operator-=(const Trainer* t) { RemoveTrainer(t); return *this; }
+	// Change mode of team: Attacking or Defending
+	const Team& operator++()                 { m_isAttacking = !m_isAttacking;    return *this; }
+
 	Player&     operator[](int index);
 	// Methods
 	void               AddPlayer(Player* p);
@@ -53,6 +68,7 @@ public:
 	const PlayerStats* GetStats()                       const { return m_playerStats;};
 	void               SetName(const char* name)              { delete []m_name; m_name = strdup(name); }
 	const char*        GetName()                        const { return m_name; }
+	bool               IsAttacking()                    const { return m_isAttacking; }
 
 	// Method overrides
 	friend std::ostream& operator<<(std::ostream& os, const Team& t);
@@ -62,6 +78,7 @@ private:
 	Trainer*     m_trainers;
 	char*        m_name;
 	PlayerStats* m_playerStats;
+	bool         m_isAttacking;
 };
 
 #endif
